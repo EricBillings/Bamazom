ff4f16b96f1ed556d11c9815d0df2eb5e568e273
Eric Billings <billings.company@gmail.com>
2019-08-27 23:09:43 -0400

    mapping cart array and appending generic divs

--- public/js/app.js
+++ public/js/app.js
@@ -23,49 +23,66 @@ $(document).ready(function () {
             <a href="#" class="btn btn-primary cartButton" id="${data[i]["id"]}">Add to Cart</a>
             </div>
             </div>`)
-            
+
 
             $("#middleContainer").append(newCard);
 
         }
 
     }
-    
+
     let cart = [];
     $("#middleContainer").on("click", ".cartButton", function () {
         console.log(this.id);
         cart.push(this.id);
-        localStorage.setItem("cartItems", cart) 
-        buildCart();       
-    })
-    
-    function buildCart () {
+        localStorage.setItem("cartItems", cart)
         console.log(cart);
-    }
+    });
 
 
+    $("#checkout").on("click", function (data) {
+        convertCart();
+        log(cart);
+        for (let i = 0; i < cart.length; i++) {
+            const prodDiv = `<div class="col-4">TEST</div>`;
+            const qtyDiv = `<div class=col-2>"</div>`;
+            const priceDiv = `<div class="col-3"></div>`;
+
+            $("#middleContainer").append(prodDiv);
 
 
+        }
+    });
 
 
+    function log(cart) {
+        console.log(cart);
+    };
 
 
+    function convertCart() {
+        cart = cart.map(Number);
+        cart = cart.map(function (item) {
+            return item -= 1;
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    };
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
